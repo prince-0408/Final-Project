f34914a00fe40eefb6647d67ce2291288c407039
Prince Yadav <66916296+prince-0408@users.noreply.github.com>
2021-10-22 11:46:31 +0530

    background image set

---
+++ AJU/Views/SignUp/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  AJU
+//
+//  Created by Anand Yadav on 21/10/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+}

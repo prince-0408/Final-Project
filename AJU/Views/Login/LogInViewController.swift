a62006ae4e0f34684ee48523c8062af83406dc8b
Prince Yadav <66916296+prince-0408@users.noreply.github.com>
2021-10-22 16:12:35 +0530

    Designed Login & SignUp Screen

--- AJU/Views/Login/LogInViewController.swift
+++ AJU/Views/Login/LogInViewController.swift
@@ -8,22 +8,27 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var rollno: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginTapped(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func forgotPasswordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func SignUpTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "SignUp")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+    }
+    
 }
--- AJU/Views/SignUp/SignUpViewController.swift
+++ AJU/Views/SignUp/SignUpViewController.swift
@@ -8,13 +8,28 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var roll: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirm: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
-
-   
+    @IBAction func SignUpTapped(_ sender: Any) {
+    }
+    
+    @IBAction func CreateAccount(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "Login")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+        
+    }
+    
 }

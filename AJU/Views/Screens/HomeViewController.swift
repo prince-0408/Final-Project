49fdbd2256ffd2f1ac3e979e68be9df6d5f95faf
Prince Yadav <66916296+prince-0408@users.noreply.github.com>
2021-12-06 17:09:13 +0530

    pod

--- AJU/Views/Screens/HomeViewController.swift
+++ AJU/Views/Screens/HomeViewController.swift
@@ -12,8 +12,9 @@ class HomeViewController: UIViewController {
     @IBAction func settingButtonTapped(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(identifier: "setting")
-        vc.modalPresentationStyle = .overFullScreen
-        present(vc, animated: true)
+        // vc.modalPresentationStyle = .overFullScreen
+       present(vc, animated: true)
+        
     }
     @IBAction func todoButtonTapped(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
